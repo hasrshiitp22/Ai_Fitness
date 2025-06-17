259135e5bb482b40b95b61b9b57f9d5b3645962f
hasrshiitp22 <hr635332@gmail.com>
2025-06-18 02:16:50 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-MONGOOSEURI='mongodb+srv://harshr82829:x964VfEYdfEobaZn@cluster0.awu9ezs.mongodb.net/IITP_PROJECT?retryWrites=true&w=majority&appName=Cluster0';
+
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGOOSEURI, {
